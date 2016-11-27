558c2f6e1cb050fbe9c4247508836f7a2a160c85
javagalbhanu <javagalbhanu@gmail.com>
2016-11-27 12:25:28 +0530

    Develpoed LoginPage

--- src/pom/BasePage.java
+++ src/pom/BasePage.java
@@ -2,6 +2,7 @@ package pom;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.PageFactory;
+import org.testng.Assert;
 
 public abstract class BasePage {
 	public WebDriver driver;
@@ -9,5 +10,10 @@ public abstract class BasePage {
 	public BasePage(WebDriver driver){
 		PageFactory.initElements(driver,this);
 		this.driver=driver;
+	}
+	
+	public void verifyTitle(String eTitle){
+		String aTitle=driver.getTitle();
+		Assert.assertEquals(aTitle, eTitle);
 	}
 }
