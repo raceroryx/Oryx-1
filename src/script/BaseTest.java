a7f8be16c3811538ea0e693311bf51373d20d745
javagalbhanu <javagalbhanu@gmail.com>
2016-12-16 18:24:50 +0530

    Latest on Friday

--- src/script/BaseTest.java
+++ src/script/BaseTest.java
@@ -4,9 +4,7 @@ import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.concurrent.TimeUnit;
 
-import org.openqa.selenium.Platform;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.AfterMethod;
